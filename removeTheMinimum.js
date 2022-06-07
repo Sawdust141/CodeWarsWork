fc471b288effbf2052b8dd8481dc32caceb2342d
Sam <samsimpson123_31@hotmail.com>
2022-06-07 06:59:23 -0700

    Daily Work

---
+++ removeTheMinimum.js
@@ -0,0 +1,12 @@
+/*Given an array of integers, remove the smallest value.
+Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index.
+If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.*/
+
+function removeSmallest(numbers) {
+    numbers = numbers.slice(0);
+    numbers.splice(numbers.indexOf(Math.min(...numbers)),1)
+    return numbers
+  }
+  
