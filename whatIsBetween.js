d13a6c0f750871b3993acd86d340151a5fd8071a
Sam <samsimpson123_31@hotmail.com>
2022-06-21 18:48:37 +0200

    Daily Work

--- whatIsBetween.js
+++ whatIsBetween.js
@@ -1,3 +1,6 @@
+/*Complete the function that takes two integers (a, b, where a < b)
+and return an array of all integers between the input parameters, including them.*/
+
 function between(a, b) {
     let answer = []
     for(i = a; i <= b; i++) {
